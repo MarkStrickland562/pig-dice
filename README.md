4ecb78328801bbeb5f90b6f68c8a2bcd4a0f5918
Kenny Halim and Mark Strickland <mark@>
2019-01-30 11:59:26 -0800

    Add working game.

---
+++ README.md
@@ -0,0 +1,11 @@
+1) Two users play Pig Dice.
+2) A single "die" is represented by an image of one of the six sides of a standard die.
+3) Users decide who goes first by rolling the die and guessing which side of the die will be displayed (1 through 6). The  user whose guess is closest to the displayed side of the die goes first.
+ User's can't use the same number for their guess.
+4) Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold".
+5) If the player rolls a 1, they score nothing and it becomes the next player's turn.
+6) If the player rolls any other number, it is added to their turn total and the player's turn continues.
+7) If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn.
+8) The first player to score 100 or more points wins.
+9) The user who wins will be shown a "Winner" page.
+10) Once either user has clicked to Play Again, the game will restart.
---
+++ js/scripts.js
@@ -0,0 +1,178 @@
+ //Business Logic for Die Sides
+function dice() {
+  this.diceSides = [];
+}
+
+dice.prototype.addDiceSide = function(diceSide) {
+  this.diceSides.push(diceSide);
+}
+
+dice.prototype.findImage = function(sideNumber) {
+  for (i=0; i < this.diceSides.length; i++) {
+    if (this.diceSides[i]){
+      if (this.diceSides[i].sideNum === sideNumber){
+        return this.diceSides[i].imageURL;
+      }
+    }
+  };
+  return false;
+}
+
+// Business Logic for Images of Sides of a Die
+function diceSide(sideNum, imageURL) {
+    this.sideNum = sideNum,
+    this.imageURL = imageURL
+}
+
+var diceImages = new dice();
+
+var sides = ["images/side1.png", "images/side2.png", "images/side3.png",
+             "images/side4.png", "images/side5.png", "images/side6.png"];
+
+for (i=1; i<= 6; i++) {
+  var side = new diceSide(i, sides[i-1]);
+  diceImages.addDiceSide(side);
+}
+
+var validateGuess = function (player1Guess, player2Guess) {
+  if (player1Guess === player2Guess) {
+    $("input#player2Guess").val("");
+    alert("Player 2's Guess Cannot be the Same as Player 1's");
+  }
+}
+
+var getFirstPlayer = function (player1Guess, player2Guess) {
+  var roll = Math.floor((Math.random() * 6) + 1);
+  console.log(roll);
+  if (roll === player1Guess) {
+    return 1;
+  }
+  else if (roll === player2Guess) {
+    return 2;
+  }
+  else if (Math.abs(roll-player1Guess) === Math.abs(roll-player2Guess)) {
+    return 1;
+  }
+  else if (Math.abs(roll-player1Guess) < Math.abs(roll-player2Guess)) {
+    return 1;
+  }
+  else {
+    return 2;
+  }
+}
+
+var rollDice = function() {
+  return Math.floor((Math.random() * 6) + 1);
+}
+
+var currentPoints = 0;
+
+var playGame = function() {
+
+  var roll = rollDice();
+  var rollImage = diceImages.findImage(roll);
+  $("#sideOfDie").html("<img src='" + rollImage + "'>");
+  //currentPoints += roll;
+  if (roll === 1) {
+    currentPoints = 0;
+    switchPlayer();
+  }
+  else {
+    currentPoints = currentPoints + roll;
+  }
+  $("#currentPoints").text(currentPoints);
+}
+
+var switchPlayer = function() {
+  if (turn === 1) {
+    turn = 2;
+  }
+  else {
+    turn = 1;
+  }
+  disableButtons();
+  enableButtons();
+}
+
+var disableButtons = function() {
+  if (turn === 1){
+    document.getElementById("player2Roll").disabled = true;
+    document.getElementById("player2Hold").disabled = true;
+
+  }
+  else {
+    document.getElementById("player1Roll").disabled = true;
+    document.getElementById("player1Hold").disabled = true;
+  }
+}
+
+var enableButtons = function() {
+  if (turn === 2){
+    document.getElementById("player2Roll").disabled = false;
+    document.getElementById("player2Hold").disabled = false;
+
+  }
+  else {
+    document.getElementById("player1Roll").disabled = false;
+    document.getElementById("player1Hold").disabled = false;
+  }
+}
+var points1 = 0;
+var points2 = 0;
+var calculateScore = function() {
+
+  if (turn === 1){
+    points2 = currentPoints + points2;
+    $("#player2Score").text(points2);
+  }
+  else {
+    points1 = currentPoints + points1;
+    $("#player1Score").text(points1);
+  }
+}
+
+function attachButtonListeners(){
+  $("#button1").on("click", "#player1Roll", function(){
+    playGame();
+  });
+  $("#button2").on("click", "#player2Roll", function(){
+    playGame();
+  });
+  $("#button1").on("click", "#player1Hold", function(){
+    turn = 2;
+    calculateScore();
+    disableButtons();
+    enableButtons();
+    $("#currentPoints").text("0");
+    currentPoints = 0;
+  });
+  $("#button2").on("click", "#player2Hold", function(){
+    turn = 1;
+    calculateScore();
+    disableButtons();
+    enableButtons();
+    $("#currentPoints").text("0");
+    currentPoints = 0;
+  });
+};
+
+var turn = 0;
+$(document).ready(function() {
+  attachButtonListeners();
+  $("form#playGame").submit(function(event) {
+    event.preventDefault();
+
+    inputtedPlayer1 = $("input#player1").val();
+    inputtedPlayer2 = $("input#player2").val();
+    inputtedPlayer1Guess = $("input#player1Guess").val();
+    inputtedPlayer2Guess = $("input#player2Guess").val();
+
+    $("#player1Name").empty().text(inputtedPlayer1);
+    $("#player2Name").empty().text(inputtedPlayer2);
+
+    validateGuess(inputtedPlayer1Guess, inputtedPlayer2Guess);
+
+    turn = getFirstPlayer(inputtedPlayer1Guess, inputtedPlayer2Guess);
+    disableButtons();
+  });
+});
